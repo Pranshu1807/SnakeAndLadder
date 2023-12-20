d630798ae2c68bbf1bd03cd264297dc7fe46fe53
Pranshu Srivastava <pranshusrivastava000@gmail.com>
2023-12-21 02:31:03 +0530

    UC4 created

--- src/App.java
+++ src/App.java
@@ -1,6 +1,17 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Welcome to Snake and Ladder Game");
         int position = 0;
+
+        while (position < 100) {
+            int randomNumber = (int) (Math.random() * 100);
+            int option = randomNumber % 3;
+            int dice = (randomNumber % 6) + 1;
+            if (option == 1) {
+                position = position + dice > 100 ? position : position + dice;
+            }
+            if (option == 2)
+                position = Math.max(0, position - dice);
+        }
+        System.out.println("The player reached the 100 mark ");
     }
 }
