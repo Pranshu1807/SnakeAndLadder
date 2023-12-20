8a3d2cd439cbe73c234b9c9478aace638e1d1883
Pranshu Srivastava <pranshusrivastava000@gmail.com>
2023-12-21 02:11:39 +0530

    UC1 created

--- src/App.java
+++ src/App.java
@@ -1,6 +1,8 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Welcome to Snake and Ladder Game");
         int position = 0;
+        int randomNumber = (int) (Math.random() * 100);
+        int dice = (randomNumber % 6) + 1;
+        System.out.println("The number in the die is " + dice);
     }
 }
