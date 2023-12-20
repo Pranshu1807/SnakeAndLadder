d4bbbb0365988c97ecff829fd36c863776c3b08a
Pranshu Srivastava <pranshusrivastava000@gmail.com>
2023-12-21 02:48:37 +0530

    UC6 created

--- src/App.java
+++ src/App.java
@@ -1,6 +1,31 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Welcome to Snake and Ladder Game");
-        int position = 0;
+        int player1 = 0;
+        int player2 = 0;
+        int chance = 0;
+        while (player1 < 100 && player2 < 100) {
+            int randomNumber = (int) (Math.random() * 100);
+            int option = randomNumber % 3;
+            int dice = (randomNumber % 6) + 1;
+            if (option == 1) {
+                if (chance == 0)
+                    player1 = player1 + dice > 100 ? player1 : player1 + dice;
+                else
+                    player2 = player2 + dice > 100 ? player2 : player2 + dice;
+            }
+            if (option == 2) {
+                if (chance == 0)
+                    player1 = Math.max(0, player1 - dice);
+                else
+                    player2 = Math.max(0, player2 - dice);
+            }
+            if (option != 1)
+                chance = 1 - chance;
+        }
+        System.out.println(player1 + " " + player2);
+        if (player1 == 100)
+            System.out.println("Player 1 won");
+        if (player2 == 100)
+            System.out.println("Player 2 won");
     }
 }
