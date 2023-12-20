f604bb5e343570ceca128c8464529e34ead7431f
Pranshu Srivastava <pranshusrivastava000@gmail.com>
2023-12-21 02:06:32 +0530

    main file created

--- src/App.java
+++ src/App.java
@@ -1,5 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Welcome to Snake and Ladder Game");
+        int position = 0;
     }
 }
