5d4768fed8f88d98fcbb1ac6819a1d98cf021f2a
Pranshu Srivastava <pranshusrivastava000@gmail.com>
2023-12-21 02:18:11 +0530

    UC2 created

--- src/App.java
+++ src/App.java
@@ -1,6 +1,15 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Welcome to Snake and Ladder Game");
         int position = 0;
+        int randomNumber = (int) (Math.random() * 100);
+        int dice = (randomNumber % 6) + 1;
+        int option = randomNumber % 3;
+        if (option == 1)
+            position += dice;
+        if (option == 2) {
+            position -= dice;
+            position = Math.max(position, 0);
+        }
+        System.out.println("The current position is " + position);
     }
 }
